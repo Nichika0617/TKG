504cea90034e18e987f83c5157c98b86143008a3
Naoru Chinen <e195734@ie.u-ryukyu.ac.jp>
2021-09-30 12:10:59 +0900

    リファクタ、マージのための一時的コミット

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 function calc(jinbun, syakai, sougou,  career, ryudai) {
+    //const jinbun = document.getElementById('jinbun');
+    //const syakai = document.getElementById('syakai');
+    const sougouryouiki = document.getElementById('sougouryouiki');
+
     if (jinbun > 2) {
-        const jinbun_value = document.getElementById('result_jinbun').value = 0;
+        document.getElementById('result_jinbun').value = 0;
     }else{
-        const jinbun_value = document.getElementById('result_jinbun').value = 2 - parseInt(jinbun);
+        document.getElementById('result_jinbun').value = 2 - parseInt(jinbun);
     }
 
     if (syakai > 2) {
-        const syakai_value = document.getElementById('result_syakai').value = 0;
+        document.getElementById('result_syakai').value = 0;
     }else{
-        const syakai_value = document.getElementById('result_syakai').value = 2 - parseInt(syakai);
+        document.getElementById('result_syakai').value = 2 - parseInt(syakai);
     }
 
-    const sum_sougouryouiki = document.getElementById('sougouryouiki').value = parseInt(sougou) + parseInt(career) + parseInt(ryudai);
+    const sum_sougouryouiki = document.getElementById('sougouryouiki').value = parseInt(sougou) + parseInt(career) + parseInt(ryudai) + 0;
 
     if (sum_sougouryouiki > 2) {
-        const sougouryouiki_value = document.getElementById('result_sougouryouiki').value = 0;
+        document.getElementById('result_sougouryouiki').value = 0;
     }else{
-        const sougouryouiki_value = document.getElementById('result_sougouryouiki').value = 2 - parseInt(sum_sougouryouiki);
+        document.getElementById('result_sougouryouiki').value = 2 - parseInt(sum_sougouryouiki);
     }
+
+    if (Japanese > 2) {
+        document.getElementById('result_Japanese').value = 0;
+    }else{
+        document.getElementById('result_Japanese').value = 2 - parseInt(sum_sougouryouiki);
+    }
+
+    
 }
